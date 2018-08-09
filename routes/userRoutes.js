174985d951a5c481865b6b21bbfbc8be45ce4c24
John-Paul Quiroga <Quiroga_jp@yahoo.com>
2018-08-09 11:29:16 -0400

    about

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,11 +3,26 @@ const userRoutes = express.Router();
 const User       = require('../models/user');
 
 
+// userRoutes.get('/aboutpage', (req, res, next) => {
+//     User.find()
+//     .then((allTheUsers)=>{
+//         res.json({allTheUsers});
+//     })
+//     .catch((err)=>{
+//         res.json(err);
+//     })
+
+// });
 
 userRoutes.get('/aboutpage', (req, res, next) => {
-    User.find()
-    .then((allTheUsers)=>{
-        res.json(allTheUsers);
+    User.find({role: 'member'})
+    .then((members)=>{
+        User.find({role: 'admin'})
+        .then((admins) => {
+            res.json({ members, admins })
+        }).catch((err) => {
+            res.json(err);
+        })
     })
     .catch((err)=>{
         res.json(err);
